1b1eadd900753a65b44cf7d33d2e68b0b84376ce
Mikhail Mishin <mishinma1805@gmail.com>
2016-02-07 20:18:49 +0300

    added units to test_simulation.py

--- tardis/simulation/tests/test_simulation.py
+++ tardis/simulation/tests/test_simulation.py
@@ -5,7 +5,7 @@ import pytest
 from tardis.io import config_reader
 from tardis.model import Radial1DModel
 from tardis.simulation import Simulation
-
+from astropy import units as u
 
 @pytest.fixture
 def tardis_config(kurucz_atomic_data, tardis_config_verysimple):
@@ -29,7 +29,7 @@ def simulation_one_loop(raw_model, tardis_config):
 @pytest.fixture()
 def simulation_compare_data_fname():
     return 'tardis/simulation/tests/data/test_data.h5'
-
+globals()
 
 @pytest.fixture()
 def simulation_compare_data(simulation_compare_data_fname):
@@ -38,25 +38,23 @@ def simulation_compare_data(simulation_compare_data_fname):
 
 def test_plasma_estimates(simulation_one_loop, simulation_compare_data):
     t_rad, w = simulation_one_loop.runner.calculate_radiationfield_properties()
-
     npt.assert_allclose(simulation_one_loop.runner.nu_bar_estimator,
                         simulation_compare_data['test1/nubar_estimators'],
                         atol=0.0)
     npt.assert_allclose(simulation_one_loop.runner.j_estimator,
                         simulation_compare_data['test1/j_estimators'],
                         atol=0.0)
-
     npt.assert_allclose(
-            t_rad, simulation_compare_data['test1/t_rad'], atol=0.0)
+            t_rad, simulation_compare_data['test1/t_rad']*u.Unit('K'), atol=0.0)
     npt.assert_allclose(w, simulation_compare_data['test1/w'], atol=0.0)
 
 
 def test_packet_output(simulation_one_loop, simulation_compare_data):
     npt.assert_allclose(
             simulation_one_loop.runner.output_nu,
-            simulation_compare_data['test1/output_nu'],
+            simulation_compare_data['test1/output_nu'] * u.Unit('Hz'),
             atol=0.0)
 
     npt.assert_allclose(simulation_one_loop.runner.output_energy,
-                        simulation_compare_data['test1/output_energy'],
+                        simulation_compare_data['test1/output_energy'] * u.Unit('erg'),
                         atol=0.0)
